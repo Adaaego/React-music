7992e0df23c3c544d7e63eee7050051b507233e7
lisa <njokuleesah@gmail.com>
2025-01-28 06:55:59 +0000

    added state for song data and individual song access, pass props to Song component, style image and headings

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import Player from './components/Player';
 import Song from './components/Song';
 import './styles/app.scss';
+import data from './util'
 
 
 
 function App() {
 
+ const[song,setSong]  = useState(data());
+ const [currentSong, setCurrentSong] = useState(song[0])
  
 
   return (
   
       <div className='App'>
-        <Song />
+        <Song currentSong ={currentSong}/>
         <Player />
     </div>
   )
--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Song = () => {
+const Song = ({currentSong}) => {
     return(
         <div className="song-container">
-            <h1>song image</h1>
-            <h2>song title</h2>
-            <h3>song artist</h3>
+            <img src={currentSong.cover}></img>
+            <h2>{currentSong.name}</h2>
+            <h3>{currentSong.artist}</h3>
         </div>
     )
 }
