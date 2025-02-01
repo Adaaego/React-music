8daba0680f52a78adc25eee5ac1234b5667a3ccc
lisa <njokuleesah@gmail.com>
2025-02-01 15:18:49 +0000

    used the ternanry operator to toggle between the play and pause icon

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React,  {useRef, useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faPlay, faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
+import {faPlay, faPause, faAngleLeft, faAngleRight} from '@fortawesome/free-solid-svg-icons';
 
 const Player = ({currentSong, isPlaying, setIsPlaying}) => {
     const audioRef =useRef(null)
@@ -56,7 +56,7 @@ const Player = ({currentSong, isPlaying, setIsPlaying}) => {
                 
                 <div className="play-control">
                 <FontAwesomeIcon className="skip-back" size="2x"  icon={faAngleLeft} />
-                <FontAwesomeIcon className="play" size="2x"  icon={faPlay} onClick={playSongHandler}/>
+                <FontAwesomeIcon className="play" size="2x"  icon={isPlaying ? faPause : faPlay} onClick={playSongHandler}/>
                 <FontAwesomeIcon className="skip-forward" size="2x"  icon={faAngleRight}/>
 
                 </div>
